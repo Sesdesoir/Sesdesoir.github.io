4d491a0b1f4e3b002d58b639bcf8eb2074f572ee
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-02-11 20:04:00 -0800

    Update script.js

--- WebDevelopmentSuccess/assets/script.js
+++ WebDevelopmentSuccess/assets/script.js
@@ -47,62 +47,13 @@ function jobSearch() {
     resultsCards.innerHTML = "";
     var searchTerm = "";
     searchTerm = document.getElementById("search-bar").value;
-    var requestUrl = 'https://cors-anywhere.herokuapp.com/jobs.github.com/positions.json?description=' + searchTerm;
-
-  // Fetch request to Github Jobs API
-  fetch(requestUrl)
-    .then(function (response) {
-      var responseStatus = response.status;
-      if (response.ok == false) {
-        resultsCards.textContent = "Error: " + responseStatus + " | Please refresh the page and try again";
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      // Loop over the returned data to create a card for each job listing
-      for (var i = 0; i < data.length; i++) {
-        // Creating all the elements within the card
-        var createCardColumn = document.createElement('div');
-        createCardColumn.className = "card column is-one-fifth";
-        createCardColumn.style.height = "250px";
-        var cardHeader = document.createElement('header');
-        cardHeader.className = "card-header";
-        var cardTitle = document.createElement('p');
-        cardTitle.className = "card-header-title is-centered";
-        var cardContent = document.createElement('div');
-        cardContent.className = "card-content";
-        var innerCardContent = document.createElement('div');
-        innerCardContent.className = "content";
-        var cardFooter = document.createElement('footer');
-        cardFooter.className = "card-footer";
-        var cardLink = document.createElement('a');
-        cardLink.className = "card-footer-item";
-
-        // Populating the cards with info about job search results
-        cardTitle.textContent = data[i].title;
-        // Some of the company URLs are not provided or, annoyingly, just "http:". This conditional links the company url only if the URL provided by the API is not empty or "http:"
-        if (data[i].company_url !== null && data[i].company_url !== "http:") {
-          innerCardContent.innerHTML = "<strong>Company: </strong><a href=" + data[i].company_url + ">" + data[i].company + "</a>"+ "<br><strong> Location: </strong>" + data[i].location + " (" + data[i].type + ")";
-        } else {
-          innerCardContent.innerHTML = "<strong>Company: </strong>" + data[i].company + "<br><strong> Location: </strong>" + data[i].location + " (" + data[i].type + ")";
-        }
-        cardLink.textContent = "Click here to learn more";
-        cardLink.href = data[i].url;
-
-
-        // Appending card elements in the correct order
-
-        resultsCards.appendChild(createCardColumn);
-        createCardColumn.appendChild(cardHeader);
-        cardHeader.appendChild(cardTitle);
-        createCardColumn.appendChild(cardContent);
-        cardContent.appendChild(innerCardContent);
-        createCardColumn.appendChild(cardFooter);
-        cardFooter.appendChild(cardLink);
+    var locationTerm = "";
+    locationTerm = document.getElementById("location-bar").value;
+    if (locationTerm !== null) {
+      var requestUrl = 'https://cors-anywhere.herokuapp.com/jobs.github.com/positions.json?description=' + searchTerm + "&location=" + locationTerm;
+    } else {
+      var requestUrl = 'https://cors-anywhere.herokuapp.com/jobs.github.com/positions.json?description=' + searchTerm;
     }
-});
-}
 
 // Skill search button call
 
