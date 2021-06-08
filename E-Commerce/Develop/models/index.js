a85c7a4a69a78e757579d400965f07a56d7d120e
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-06-07 23:38:06 -0700

    Learned something new "through"

--- E-Commerce/Develop/models/index.js
+++ E-Commerce/Develop/models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
-
+      // ::belongs to many:: we're including table 2 with table 1 and they're linked through table 3.
+      //...so Tag is being included with Product and they're linked through ProductTag table
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
