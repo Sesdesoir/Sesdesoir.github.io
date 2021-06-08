2aea21d86488567eeb11931376243cf08d986945
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-06-07 23:33:34 -0700

    Wanted webpage functionality

--- E-Commerce/Develop/routes/index.js
+++ E-Commerce/Develop/routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send(`<h1>Wrong Route!</h1>\n <br><ul><li><a href="http://localhost:3001/api/products" >All Products</a></li><li><a href="http://localhost:3001/api/categories" >All Categories</a></li><li><a href="http://localhost:3001/api/tags" >All Tags</a></li></ul>`)
 });
 
 module.exports = router;
