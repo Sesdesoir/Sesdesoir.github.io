095afbcb4d9c88022947070d4d621e31037668b6
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-06-07 23:44:26 -0700

    Added USE

--- E-Commerce/Develop/db/schema.sql
+++ E-Commerce/Develop/db/schema.sql
@@ -1,6 +1,7 @@
--- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
-
--- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+-- I like inserting USE even if sequelize takes care of it
+--I basically copy pasta schema into my workbench and it makes me feel better to use the db before I seed
+
 
